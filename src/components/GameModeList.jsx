76b1469e8c77fff5286c9f26a98971bbb90e2f65
maba <iq1359@naver.com>
2025-08-07 14:29:19 +0900

    feat: 게임모드 리스트(GameModeList) 컴포넌트 추가

---
+++ src/components/GameModeList.jsx
@@ -0,0 +1,20 @@
+import MatchCard from './MatchCard';
+import { gameModes } from '../data/gameMode';
+
+function GameModeList() {
+  return (
+    <div className="flex justify-center gap-10">
+      {gameModes.map((mode) => (
+        <MatchCard
+          key={mode.id}
+          title={mode.title}
+          subtitle={mode.subtitle}
+          imageSrc={mode.image}
+          Icon={mode.icon}
+          color={mode.color}
+        />
+      ))}
+    </div>
+  );
+}
+export default GameModeList;
