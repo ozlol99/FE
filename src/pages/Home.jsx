2da1639f6b2bdf93d07f851d6d47f38a0e2abda0
maba <iq1359@naver.com>
2025-08-07 14:28:00 +0900

    feat: 홈 화면에 GameModeList 컴포넌트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import GameModeList from '../components/GameModeList';
+
 function Home() {
   return (
     <>
-      <></>
+      <div className="w-full h-[420px] flex items-center justify-center">
+        <GameModeList />
+      </div>
     </>
   );
 }
-
 export default Home;
