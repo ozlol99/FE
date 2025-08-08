2776b61aceca29cfac11364c6037ca44556f8126
gunysunny <mikan31@naver.com>
2025-08-08 18:37:29 +0900

    feat: 로그아웃 액션 추가 및 마이페이지 링크 연결

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,14 @@ import NavBarLinkStyle from './NavBarLinkStyle';
 import Logo from '../assets/logo.svg?react';
 import { useNavigate } from 'react-router-dom';
 
-function NavBar({ isLogin }) {
+function NavBar({ isLogin, onLogout }) {
   const navItems = [
     { label: '팀원 찾기', path: '/' },
     { label: '랭킹', path: '/' },
     { label: '챔피언', path: '/' },
   ];
   const navigate = useNavigate();
+
   return (
     <div className="w-screen h-[90px] bg-[#232323] text-white flex items-center px-6">
       <div className="flex items-center justify-center text-white font-bold text-2xl h-full w-[100px] pr-6">
@@ -21,9 +22,14 @@ function NavBar({ isLogin }) {
           </NavBarLinkStyle>
         ))}
       </div>
-      <div className="flex h-full ml-auto ">
+      <div className="flex h-full ml-auto gap-2">
+        {' '}
+        {/* ← gap으로 간격 */}
         {isLogin ? (
-          <NavBarLinkStyle to="/login">마이페이지</NavBarLinkStyle>
+          <>
+            <NavBarLinkStyle to="/mypage">마이페이지</NavBarLinkStyle>
+            <NavBarLinkStyle onClick={onLogout}>로그아웃</NavBarLinkStyle>
+          </>
         ) : (
           <NavBarLinkStyle to="/login">로그인</NavBarLinkStyle>
         )}
