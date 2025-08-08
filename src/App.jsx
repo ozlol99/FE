bf9cb1838223f2ed710967299bc0196bef1cc373
gunysunny <mikan31@naver.com>
2025-08-08 18:23:58 +0900

    feat: /mypage 라우트 추가 및 로그인 상태(isLogin) 도입

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Layout from './Layouts/layout';
+import Layout from './layouts/Layout';
 import { useState } from 'react';
 import OAuthCallback from './pages/OAuthCallback';
 import AddInfo from './pages/AddInfo';
+import MyPage from './pages/MyPage';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [isLogin, _setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <>
-      <Routes>
-        <Route element={<Layout isLogin={isLogin} />}>
-          <Route path="/" element={<Home />} />
-        </Route>
+    <Routes>
+      <Route element={<Layout isLogin={isLogin} setIsLogin={setIsLogin} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/auth/callback" element={<OAuthCallback />} />
         <Route path="/add-info" element={<AddInfo />} />
-      </Routes>
-    </>
+        <Route path="/auth/callback" element={<OAuthCallback />} />
+      </Route>
+    </Routes>
   );
 }
-
 export default App;
