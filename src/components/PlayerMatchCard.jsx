c0f92bcd85cf8cbcfe39c815564e18f85418d624
maba <iq1359@naver.com>
2025-08-12 14:28:02 +0900

    feat: PlayerMatchCard 컴포넌트 구현 및 매치 상세 UI 구성

--- src/components/PlayerMatchCard.jsx
+++ src/components/PlayerMatchCard.jsx
@@ -3,10 +3,12 @@ import { formatGameDuration, timeAgoTimestamp } from '../utils/timeFormat';
 import { positionIcons } from '../data/positionIcons';
 import { getChampionImg, getRuneImg, getSpellImg } from '../data/imageCDN';
 
-function PlayerMatchCard({ matchesInfo, puuid }) {
-  if (!puuid || !matchesInfo) return null;
+function PlayerMatchCard({ match, puuid }) {
+  if (!puuid || !match) return null;
 
-  const match = Array.isArray(matchesInfo) ? matchesInfo[0] : matchesInfo;
+  //매치정보가 하나만 넘어올때 => 객체 , 여러개로 올떼 베열
+  //   const match = Array.isArray(matchesInfo) ? matchesInfo[0] : matchesInfo;
+  const info = match?.info;
   const myData = match?.info?.participants.find((p) => p.puuid === puuid);
 
   console.log('--------------------------------------');
@@ -22,17 +24,19 @@ function PlayerMatchCard({ matchesInfo, puuid }) {
   console.log(myData.perks?.styles?.[0]?.selections?.[0]);
 
   return (
-    <div className=" relative flex w-full border-l-8 border-l-red-600 rounded-xl min-h-[110px] ">
+    <div className=" relative flex w-full border-l-8 border-l-rose-600 rounded-xl min-h-[110px] ">
       <div className=" absolute w-full h-full top-0 inset-0 bg-red-400/40 -z-10"></div>
-      <div className="flex flex-col flex-1 w-20 p-1 gap-1 text-xs justify-center ">
+      <div className="flex flex-col flex-1 w-20 p-1 gap-1 text-xs justify-center pl-2">
         <div>개인 /2인 랭크</div>
-        <div>{timeAgoTimestamp(matchesInfo?.info?.gameEndTimestamp)}</div>
+        <div>
+          {timeAgoTimestamp(info?.gameEndTimestamp ?? info?.gameStartTimestamp)}
+        </div>
         <div className="w-10 border border-stone-500"></div>
-        <div>승리</div>
-        <div>{formatGameDuration(matchesInfo?.info?.gameDuration)}</div>
+        <div>{myData.win ? '승리' : '패배'}</div>
+        <div>{formatGameDuration(info?.gameDuration)}</div>
       </div>
       <div className="flex flex-col flex-1 w-20 p-1 gap-1 text-xs justify-center ">
-        <Icon className="w-6 h-6 fill-white" />
+        <Icon className="w-6 h-6 fill-white  border border-white" />
       </div>
       <div className="flex flex-col flex-1 w-20 p-1 gap-1 text-xs justify-center ">
         <div className="flex ">
