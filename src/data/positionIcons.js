fc4b10bc5daa7d2f2375f49472cb18a5d98df21d
maba <iq1359@naver.com>
2025-08-12 12:51:55 +0900

    feat: 포지션별 SVG 아이콘 매핑 데이터 추가

---
+++ src/data/positionIcons.js
@@ -0,0 +1,13 @@
+import TopIcon from '../assets/positions/top.svg?react';
+import JungleIcon from '../assets/positions/jungle.svg?react';
+import MidIcon from '../assets/positions/mid.svg?react';
+import BotIcon from '../assets/positions/bot.svg?react';
+import SupportIcon from '../assets/positions/support.svg?react';
+
+export const positionIcons = {
+  TOP: TopIcon,
+  JUNGLE: JungleIcon,
+  MIDDLE: MidIcon,
+  BOTTOM: BotIcon,
+  UTILITY: SupportIcon,
+};
