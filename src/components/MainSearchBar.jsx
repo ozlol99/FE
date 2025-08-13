d44c2e9b5e6015322a1f5613112e8b9cb9f2bf06
maba <iq1359@naver.com>
2025-08-13 11:40:08 +0900

    feat: 네브바에 서치바 추가 및 검색 로직 수정 - 메인 페이지 제외한 페이지에서만 검색창 표시 - 엔터 입력 시 match-detail 페이지로 이동 - 기존 디바운스 로직 제거

--- src/components/MainSearchBar.jsx
+++ src/components/MainSearchBar.jsx
@@ -1,19 +1,29 @@
 import SearchIcon from '@/assets/searchIcon.svg?react';
-import { useEffect, useState } from 'react';
-import { useDebounce } from '../hooks/useDebounce';
+import { useState } from 'react';
+// import { useDebounce } from '../hooks/useDebounce';
 import { useNavigate } from 'react-router-dom';
 
 function MainSearchBar() {
   const [query, setQuery] = useState('');
-  const debouncedQuery = useDebounce(query, 1000); // 1초 후 반영
+  // const debouncedQuery = useDebounce(query, 1000);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (debouncedQuery.trim()) {
-      console.log('검색 실행:', debouncedQuery);
+  const handleSearch = () => {
+    const raw = query.trim();
+    if (!raw) return;
+    const [name, tag] = raw.split('#');
+
+    navigate(
+      `/match-detail/${encodeURIComponent(name)}/${encodeURIComponent(tag)}`,
+    );
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.nativeEvent.isComposing) {
+      handleSearch();
     }
-  }, [debouncedQuery, navigate]);
-  const [name, tag] = query.split('#');
+  };
+
   return (
     <>
       <div className="w-[700px] h-[48px] bg-[#D9D9D9] rounded-4xl border-4 border-[#a9a9a9] flex items-center justify-end overflow-hidden">
@@ -22,14 +32,11 @@ function MainSearchBar() {
           placeholder="소환사를 검색해 주세요"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <div
           className="w-[72px] h-full bg-[#00BBA3] flex items-center justify-center cursor-pointer"
-          onClick={() =>
-            navigate(
-              `/match-detail/${encodeURIComponent(name)}/${encodeURIComponent(tag)}`,
-            )
-          }
+          onClick={handleSearch}
         >
           <SearchIcon />
         </div>
--- src/components/NavSearchBar.jsx
+++ src/components/NavSearchBar.jsx
@@ -1,5 +1,46 @@
+import { SearchIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+// import { useDebounce } from '../hooks/useDebounce';
+import { useState } from 'react';
+
 function NavSearchBar() {
-  return <div>NavSearchBar</div>;
+  const [query, setQuery] = useState('');
+  // const debouncedQuery = useDebounce(query, 1000);
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    const raw = query.trim();
+    if (!raw) return;
+    const [name, tag] = raw.split('#');
+
+    navigate(
+      `/match-detail/${encodeURIComponent(name)}/${encodeURIComponent(tag)}`,
+    );
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.nativeEvent.isComposing) {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-[500px] h-[40px] bg-[#D9D9D9] rounded-4xl border-2 ">
+      <input
+        className="flex-1 h-full px-4 bg-transparent outline-none text-black font-medium text-sm"
+        placeholder="소환사를 검색해 주세요"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <div
+        className="w-10 text-stone-500 hover:text-stone-950 border-l-2 pl-2 pr-2"
+        onClick={handleSearch}
+      >
+        <SearchIcon />
+      </div>
+    </div>
+  );
 }
 
 export default NavSearchBar;
