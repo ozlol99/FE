0212ebc02210156f157935b7eba2c15ac8dbe466
maba <iq1359@naver.com>
2025-08-14 11:45:39 +0900

    feat: ChampionSidebar 컴포넌트 구현
    - 챔피언 검색(ChampionSearchBox), 역할 필터(ChampionRoleFilter), 챔피언 목록(ChampionList) 조합
    - 검색어와 선택된 역할 상태 관리

--- src/components/ChampionSidebar.jsx
+++ src/components/ChampionSidebar.jsx
@@ -1,13 +1,45 @@
-import ChampionGrid from './ChampionGrid';
+import { useMemo, useState } from 'react';
+import { matchLaneByTags } from '../data/roleToLane';
 import ChampionRoleFilter from './ChampionRoleFilter';
 import ChampionSearchBox from './ChampionSearchBox';
+import { useDebounce } from '../hooks/useDebounce';
+import ChampionList from './ChampionList';
 
-function ChampionSidebar() {
+function ChampionSidebar({ champions, selectedId, onSelect }) {
+  const [lane, setLane] = useState('ALL'); // 포지션 상태
+  const [search, setSearch] = useState('');
+
+  const debouncedSearch = useDebounce(search, 300); // 0.3초 대기 후 반영
+
+  const filtered = useMemo(() => {
+    return champions
+      .filter((ch) => matchLaneByTags(ch.tags, lane))
+      .filter(
+        (ch) =>
+          ch.name.toLowerCase().includes(debouncedSearch.toLowerCase()) ||
+          ch.id.toLowerCase().includes(debouncedSearch.toLowerCase()),
+      );
+  }, [champions, lane, debouncedSearch]);
+
   return (
-    <div className=" w-full h-[20%] min-h-[500px] md:h-full md:w-[300px] bg-red-400 p-3 ">
-      <ChampionSearchBox />
-      <ChampionRoleFilter />
-      <ChampionGrid />
+    <div className=" w-full md:w-[300px]  p-3 ">
+      <div className="block md:hidden h-[150px]  overflow-y-auto">
+        <ChampionSearchBox value={search} onChange={setSearch} />
+        <ChampionList
+          champions={filtered}
+          selectedId={selectedId}
+          onSelect={onSelect}
+        />
+      </div>
+      <div className="hidden md:block">
+        <ChampionSearchBox value={search} onChange={setSearch} />
+        <ChampionRoleFilter value={lane} onChange={setLane} />
+        <ChampionList
+          champions={filtered}
+          selectedId={selectedId}
+          onSelect={onSelect}
+        />
+      </div>
     </div>
   );
 }
